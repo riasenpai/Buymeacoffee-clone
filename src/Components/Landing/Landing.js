a41cce51a430cfb688f2cbc178ab1447b11242ff
ria <riatyagi28@gmail.com>
2021-03-11 20:52:34 +0530

    routing added

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -3,6 +3,12 @@ import styles from "./Landing.module.css";
 import top from "../../assests/top.webp";
 import support from "../../assests/support.webp";
 import dash from "../../assests/dash.webp";
+import pewds from "../../assests/pewdiepie.jpg";
+import valk from "../../assests/rae.jpg";
+import toast from "../../assests/toast.jpg";
+import jade from "../../assests/quat.jpg";
+import { Link } from "react-router-dom";
+
 class Landing extends React.Component {
   constructor(props) {
     super(props);
@@ -32,73 +38,71 @@ class Landing extends React.Component {
 
         <img className={styles.intro2} src={dash} />
         <div className={styles.generalHeading}>
-          Support your favourite creator!
+          Support your favourite streamer!
         </div>
-      
+        <Link to="/profile">
         <div className={styles.container}>
           <div className={styles.card}>
             <div className={styles.imgBox}>
-              <img src={top}></img>
+              <img src={pewds}></img>
             </div>
             <div className={styles.content}>
-              <h2>Card one</h2>
+              <h2>Pewdiepie</h2>
               <p>
-                Buy Me a Coffee is a simple, meaningful way to fund your
-                creative work. Without stitching together a bunch of apps like
-                Patreon, Mailchimp, and a donate button — you can accept
-                support, memberships, and build a direct relationship with your
-                fans. Your fans are going to love it.
+                Felix Arvid Ulf Kjellberg, known online as PewDiePie, is a
+                Swedish YouTuber, gamer and comedian, known primarily for his
+                Let's Play videos and comedic formatted shows.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.imgBox}> 
+              <img src={valk}></img>
+            </div>
+            <div className={styles.content}>
+              <h2>Valkyrae</h2>
+              <p>
+                Rachel Hofstetter, better known by her online alias Valkyrae, is
+                an American internet personality. The recipient of a Game Award,
+                she is best known for her livestreams on YouTube, being one of
+                the most viewed streamers on the platform.
               </p>
             </div>
           </div>
           <div className={styles.card}>
             <div className={styles.imgBox}>
-              <img src={top}></img>
+              <img src={toast}></img>
             </div>
             <div className={styles.content}>
-              <h2>Card two</h2>
+              <h2>Disguised Toast</h2>
               <p>
-                Buy Me a Coffee is a simple, meaningful way to fund your
-                creative work. Without stitching together a bunch of apps like
-                Patreon, Mailchimp, and a donate button — you can accept
-                support, memberships, and build a direct relationship with your
-                fans. Your fans are going to love it.
+                Jeremy Wang, better known by the alias Disguised Toast, is a
+                Taiwanese-Canadian streamer, YouTuber, and Internet personality.
+                He got his start on YouTube, creating videos about the digital
+                card game Hearthstone.
               </p>
             </div>
           </div>
           <div className={styles.card}>
             <div className={styles.imgBox}>
-              <img src={top}></img>
+              <img src={jade}></img>
             </div>
             <div className={styles.content}>
-              <h2>Card three</h2>
+              <h2>QuaterJade</h2>
               <p>
-                Buy Me a Coffee is a simple, meaningful way to fund your
-                creative work. Without stitching together a bunch of apps like
-                Patreon, Mailchimp, and a donate button — you can accept
-                support, memberships, and build a direct relationship with your
-                fans. Your fans are going to love it.
+                Social media star who has gained fame for her QuarterJade Twitch
+                channel. She has risen to massive popularity for her original
+                vlogging alongside gaming playthroughs and live streaming,
+                including for PlayerUnknown's Battlegrounds.
               </p>
             </div>
           </div>
-          {/* <div className={styles.card}>
-            <div className={styles.imgBox}>
-              <img src={top}></img>
-            </div>
-            <div className={styles.content}>
-              <h2>Card four</h2>
-              <p>
-                Buy Me a Coffee jsdjsdja a simple, meaningful way to fund your
-                creative work. Without stitching together a bunch of apps like
-                Patreon, Mailchimp, and a donate button — you can accept
-                support, memberships, and build a direct relationship with your
-                fans. Your fans are going to love it.
-              </p>
-            </div>
-          </div> */}
         </div>
+        </Link>
+        <div className={styles.generalHeading}>
+            
         </div>
-      
+      </div>
     );
   }
 }
--- src/Components/Maincomponent/MainComponent.js
+++ src/Components/Maincomponent/MainComponent.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import styles from './MainComponent.module.css';
-import Landing from './../Landing/Landing';
+import React from "react";
+import styles from "./MainComponent.module.css";
+import Landing from "./../Landing/Landing";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Profile from "./../Profile/Profile";
 
-class MainComponent extends React.Component{
-    constructor(props) {
-        super(props);
-      }
-      render(){
-        return(
-             <div>
-               <Landing></Landing>
-             </div>
-         );
-      }
-    }
-    export default MainComponent;
+
+class MainComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/profile">
+              <Profile></Profile>
+            </Route>
+            <Route path="/">
+              <Landing></Landing>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+export default MainComponent;
